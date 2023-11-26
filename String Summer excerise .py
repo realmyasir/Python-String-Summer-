c232dd7f081f827612307f15185e848c43561e75
Muhammad Yasir <145558820+realmyasir@users.noreply.github.com>
2023-11-26 12:36:06 +0500

    Add files via upload

---
+++ String Summer excerise .py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 10:00:04 2023
+
+@author: Web 123
+"""
+
+# guess=input('I\'m thinking of a word, can you guess what it is? Hint it\'s a Season >>')
+# guess = guess.lower()
+# if guess == 'summer':
+#     print('Yes it\'s Summer: Well Done')
+# elif guess == 'winter':
+#     print('No it\'s not winer: sorry')
+# elif guess == 'autum':
+#     print('No it\'s Not Autum: Sorry')
+# elif guess == 'spring':
+#     print('No it\s not Spring: Sorry')
+# else :
+#     print(guess.capitalize(),'is not a Season')
+    
+        
+# name = input('Enter Your Name Minimum Five Characters >>>')
+# name_Len = len(name)
+# if name_Len >= 5 :
+#     print('Your Name Of Length is ',name_Len,'Characters')
+# else :
+#     print('Your Name of Characters is Soo Short')
+    
+# n1 = int(input('Please Enter the Intger Of Number 1-20 >>>'))
+# n2 = int(input('Please Enter the Intger Of Number 1-20 >>>'))
+# if n1 > 15 and n2 > 15 :
+#     print('Your Two Number Of Multiply Is ',n1*n2)
+    
+# elif n1 > 15 or n2 > 15 :
+#     print('Your Two Number Of Sum Is ',n1 + n2)    
+    
+# else:
+#     print(0)
+
+# n1 = int(input('Enter Your First Interger>>'))
+# n2 = int(input('Enter Your Second Integer>>'))
+# print('Before Swaping variable Value',n1,n2)
+# n1,n2=n2,n1
+# print('After Swaping Varibales Value',n1,n2)
+
